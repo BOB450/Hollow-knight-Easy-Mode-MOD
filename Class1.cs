3448c1b95191eb3b8101836481b6e4e95ed88a6b
BOB450 <bwvoskuil@gmail.com>
2021-03-01 11:39:09 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Modding;
+
+namespace HollowModEasy
+{
+    public class Class1 : Mod
+    {
+        public Class1() : base("Easy Mode By BOB450") { }
+
+        public override void Initialize()
+        {
+            Log("Inits");
+            //ModHooks.Instance.RecordKillForJournalHook += OnKill;
+            // ModHooks.Instance.TakeHealthHook += OnHealthTaken;
+            ModHooks.Instance.NewGameHook += NewGame;
+           
+
+        }
+
+        private void NewGame()
+        {
+            PlayerData.instance.AddToMaxHealth(3);
+        }
+    }
+}
+//By BOB450
